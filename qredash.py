24260e585e133bdf58c3807ce2196b4f712fed2d
benjaminbaker-dev <benjaminbakercty@gmail.com>
2020-04-28 12:49:57 +0300

    added doctsrs and helpful comments

--- qredash.py
+++ qredash.py
@@ -5,8 +5,8 @@ import requests
 from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option
 
 REDASH_HOST = "http://localhost:5000"
-API_KEY = "4fkpGAMpJzWknTpEGXgK7c5OChpbfB2L7KkXtizI"
-POLL_INTERVAL = 3
+API_KEY = "4fkpGAMpJzWknTpEGXgK7c5OChpbfB2L7KkXtizI"  # MUST BE A USER API KEY
+POLL_INTERVAL = 3  # HOW OFTEN TO POLL REDASH TO SEE IF QUERY JOB FINISHED
 
 
 @Configuration()
@@ -20,20 +20,20 @@ class QRedash(GeneratingCommand):
     )
 
     def generate(self):
+        """
+        generates authorized http session, runs query, gets results and creates event for each 'row'
+        """
         redash_session = type(self)._create_redash_session()
         refresh_data = type(self)._refresh_query(redash_session, self.query_id)
-
         if refresh_data.status_code != 200:
             raise ValueError("Could not refresh query {}".format(self.query_id))
 
         refresh_job = refresh_data.json()['job']
         result_id = type(self)._poll_for_new_result(redash_session, refresh_job)
-
         if not result_id:
             raise ValueError("Query execution failed")
 
         results_data = type(self)._get_fresh_results(redash_session, self.query_id, result_id)
-
         if results_data.status_code != 200:
             raise ValueError("Failed fetching result id {}".format(result_id))
 
@@ -43,11 +43,19 @@ class QRedash(GeneratingCommand):
 
     @staticmethod
     def _get_fresh_results(session, query_id, result_id):
+        """
+        gets result json query job generated
+        :return response object
+        """
         response = session.get('{}/api/queries/{}/results/{}.json'.format(REDASH_HOST, query_id, result_id))
         return response
 
     @staticmethod
     def _poll_for_new_result(session, job):
+        """
+        checks status of query job, and returns result id if successfull
+        :return result_id string (or None)
+        """
         while job['status'] not in (3, 4):
             response = session.get('{}/api/jobs/{}'.format(REDASH_HOST, job['id']))
             job = response.json()['job']
@@ -59,11 +67,20 @@ class QRedash(GeneratingCommand):
 
     @staticmethod
     def _refresh_query(session, query_id):
+        """
+        before accessing results, query must be run.
+        :return response object
+        """
         resp = session.post('{}/api/queries/{}/refresh'.format(REDASH_HOST, query_id))
         return resp
 
     @staticmethod
     def _create_redash_session():
+        """
+        Redash supports two kinds of API keys. For this command, we need a User API key,
+        so that we can access multiple queries
+        :return: session object
+        """
         session = requests.Session()
         session.headers.update({'Authorization': 'Key {}'.format(API_KEY)})
         return session
